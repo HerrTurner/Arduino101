44db8b9ed5fa53c6a4494fdb67353f9a440bf7a3
George Esc <georgeeturnere@gmail.com>
2023-11-07 01:13:44 +0100

    Include GPIO examples

--- Elementos de programación/Hello_World/Hello_World.ino
+++ Elementos de programación/Hello_World/Hello_World.ino
@@ -1,15 +1,1 @@
-void setup() {
-  // En esta sección se encuentra el código que se ejecuta una vez al inicio del programa
-
-  //Iniciamos el monitor serial
-  Serial.begin(9600);
-  //Escribimos en el monitor serial
-  Serial.println("Hello World");
-
-}
-
-void loop() {
-  // En esta sección se escribie el código que se va a ejecutar en ciclo mientras el
-  // programa esté corriendo
-
-}
+Ejemplo de entradas y salidas digitales y analógicasEjemplo de entradas y salidas digitales y analógicas
